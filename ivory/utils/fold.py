b1499d0422a2ed55b44cf28b783b52743aa94234
daizutabi <daizutabi@gmail.com>
2020-05-10 13:48:00 +0900

    groupkfold

--- ivory/utils/fold.py
+++ ivory/utils/fold.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.model_selection import GroupKFold, KFold, StratifiedKFold
 
 
 def fold_array(splitter, x, y=None, groups=None):
@@ -12,6 +12,11 @@ def fold_array(splitter, x, y=None, groups=None):
 def kfold_split(x, n_splits=5, shuffle=True, random_state=0):
     splitter = KFold(n_splits, shuffle=shuffle, random_state=random_state)
     return fold_array(splitter, x)
+
+
+def group_kfold_split(groups, n_splits=5):
+    splitter = GroupKFold(n_splits)
+    return fold_array(splitter, groups, groups, groups=groups)
 
 
 def stratified_kfold_split(y, n_splits=5, shuffle=True, random_state=0):
