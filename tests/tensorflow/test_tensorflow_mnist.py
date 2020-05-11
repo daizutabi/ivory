8fdf50cd98a2f679821d5a3a197451c8ef459082
daizu <daizutabi@gmail.com>
2020-05-11 13:15:25 +0900

    tensorflow memory growth in pytest.

--- tests/tensorflow/test_tensorflow_mnist.py
+++ tests/tensorflow/test_tensorflow_mnist.py
@@ -1,9 +1,14 @@
 import pytest
+import tensorflow as tf
 
 from ivory.tensorflow.trainer import Trainer
 
 
 def test_mnist(client):
+    gpus = tf.config.experimental.list_physical_devices("GPU")
+    if gpus:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
     run = client.create_run("mnist")
     run.start("train")
     run.start("test")
