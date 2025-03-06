7642132c0245e8e9b6311153708e52030f91246a
Daizu <daizutabi@gmail.com>
2025-03-06 01:29:39 +0000

    Refactor module configuration and remove example script
    - Update pyproject.toml to change module name from `_internal` to `ivory`
    - Remove `run.py` example script
    - Delete `rust-toolchain.toml` and `_internal.pyi` files
    - Modify `src/lib.rs` to use `ivory` module name and simplify module initialization

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@ use pyo3::prelude::*;
 use pyo3_polars::PolarsAllocator;
 
 #[pymodule]
-fn _internal(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
-    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+fn ivory(_py: Python, _m: &Bound<PyModule>) -> PyResult<()> {
     Ok(())
 }
 
